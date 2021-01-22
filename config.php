2e287a224998825a11ede7fe88de276b1e7b1039
czerlun <45076422+czerlun@users.noreply.github.com>
2021-01-22 19:08:25 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'oavguynjwafqjp';
-const PASSWORD = 'a4b37aad71eb06e80f6865fb9d1d5505f6a931d41887d0b1118ff859339b67bb';
-const HOST = 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'dekpvdl0h0n5b6';
